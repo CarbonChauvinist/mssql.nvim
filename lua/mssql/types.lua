1aade6b9477b378c58a720e12d9a7bdc3c56404a
Gary Host <21365869+CarbonChauvinist@users.noreply.github.com>
2026-07-24 21:50:51 -0400

    fix(types): align MssqlConnectionOptions type annotations with objectFilters property
    Fix type mismatch in type definition - ensure uses camelCase property
    for objectFilters.

--- lua/mssql/types.lua
+++ lua/mssql/types.lua
@@ -136,14 +136,14 @@
 
 ---@class MssqlObjectFilter
 ---@field allow string[]? List of patterns or strings (e.g. "dbo.*", "sales.User*", "special_schema.single_table") to allow
----@field deny string[]? List of patterns to deny. Deny takes precendence.
+---@field deny string[]? List of patterns to deny. Deny takes precedence.
 
 ---@class MssqlConnectionOptions
 ---@field server? string
 ---@field database? string
 ---@field databaseAllowList? string[]
 ---@field databaseDenyList? string[]
----@field object_filters table<string, MssqlObjectFilter>? Keys are 't', 'v', 'sp', 'f'
+---@field objectFilters table<string, MssqlObjectFilter>? Keys are 't', 'v', 'sp', 'f'
 ---@field user? string
 ---@field password? string
 ---@field authentication? string
