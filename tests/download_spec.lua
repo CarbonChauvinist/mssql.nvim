920a9a5f806bffba7969bc03f573b31224187a1e
Gary Host <21365869+CarbonChauvinist@users.noreply.github.com>
2026-08-07 20:31:00 -0400

    test(tools): remove dangling timer from download test spec
    Eliminate obsolete defer_fn timeout timer from download_spec in favor of
    sequential coroutine execution.

--- tests/download_spec.lua
+++ tests/download_spec.lua
@@ -1,16 +1,7 @@
-local mssql = require("mssql")
 local test_utils = require("tests.utils")
 
-local iif = function(cond, true_value, false_value)
-	if cond then
-		return true_value
-	else
-		return false_value
-	end
-end
-
 local tools_folder = vim.fs.joinpath(vim.fn.stdpath("data"), "mssql.nvim/sqltools")
-local tools_file = iif(jit.os == "Windows", "MicrosoftSqlToolsServiceLayer.exe", "MicrosoftSqlToolsServiceLayer")
+local tools_file = "MicrosoftSqlToolsServiceLayer"
 
 local function tools_file_exists()
 	local f = io.open(vim.fs.joinpath(tools_folder, tools_file), "r")
@@ -35,13 +26,7 @@ return {
 		vim.fn.delete(tools_folder, "rf")
 		vim.fn.delete(vim.fs.joinpath(vim.fn.stdpath("data"), "mssql.nvim/config.json"))
 
-		local download_finished = false
-		vim.defer_fn(function()
-			assert(download_finished, "Download did not complete")
-		end, 120000)
-
 		test_utils.setup_mssql_async()
-		download_finished = true
 		assert(tools_file_exists(), "The sql server tools file does not exist among the downloads")
 	end,
 }
